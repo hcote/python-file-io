2507f97b5fef1880b2ebd5c965d3ef6537488089
hcote <hcote12@gmail.com>
2018-09-11 17:21:20 -0700

    first commit

---
+++ fileio.py
@@ -0,0 +1,21 @@
+# https://www.youtube.com/watch?v=Uh2ebFW8OYM
+
+#f = open("test.txt", "r")
+#f = open("test.txt", "w")
+#f = open("test.txt", "a")
+#f = open("test.txt", "r+")
+#print(f.name)
+#print(f.mode)
+#f.close()
+
+##Reading Files:
+#with open("test.txt", "r") as f:
+	#pass
+
+	##Small Files:
+	#f_contents = f.read()
+	#print(f_contents)
+
+	##Big Files:
+	#f_contents = f.readlines()
+	#print(f_contents)
